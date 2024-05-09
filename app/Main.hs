bb051cc2f95cb7d24bbdfaa1046d02abc02f5b82
Chris Henson <chrishenson.net@gmail.com>
2024-05-09 17:29:05 -0400

    rm old examples

--- app/Main.hs
+++ app/Main.hs
@@ -10,18 +10,6 @@ import Lambda
 main :: IO ()
 main =
   do
-    let example_1 = [ex| (λf. λx . f x) (λx. x) z|]
-    putStrLn $ pp example_1
-    putStrLn $ pp $ call_by_value example_1
-    putStrLn ""
-
-    let var_replace = V "x"
-    let replacement = [ex| y z|]
-    let body = [ex| λy. x y|]
-    putStrLn $ "[" ++ vs var_replace ++ " -> " ++ (pp replacement) ++ "](" ++ pp body ++ "]"
-    putStrLn $ pp (subst body var_replace replacement)
-    putStrLn ""
-
     let scc = [ex|λn. λs. λz. s (n s z)|]
     let plus = [ex|λm. λn. λs. λz. m s (n s z)|]
 
@@ -29,6 +17,6 @@ main =
     let one = full_beta [ex| $e:scc $e:zero |]
     let two = full_beta [ex| $e:scc $e:one |]
     let three = full_beta [ex| $e:scc $e:two |]
-
+    
     print $ assert (one == [ex| λs. λz. s z |]) one
-    print $ assert (three == full_beta [ex| $e:plus $e:one $e:two|]) three
+    print $ assert (three == full_beta [ex| $e:plus $e:one $e:two|]) three    
