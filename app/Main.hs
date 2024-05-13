f49a67862f7564771fcbb3fea477005d764720a4
Chris Henson <chrishenson.net@gmail.com>
2024-05-13 03:32:53 -0400

    another example

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,18 @@ import Control.Exception (assert)
 import Eval
 import Lambda
 
+toChurch :: Int -> Exp
+toChurch 0 = [ex| λs. λz. z|]
+toChurch n = full_beta [ex| $e:scc $e:prev |]
+  where
+    scc = [ex|λn. λs. λz. s (n s z)|]
+    prev = toChurch $ n - 1
+
+fromChurch :: Exp -> Maybe Int
+fromChurch [ex| λs. λz. z|] = Just 0
+fromChurch [ex| λs. λz. s $e:e|] = (+1) <$> fromChurch [ex| λs. λz. $e:e|]
+fromChurch _ = Nothing
+
 main :: IO ()
 main =
   do
@@ -19,4 +31,7 @@ main =
     let three = full_beta [ex| $e:scc $e:two |]
     
     print $ assert (one == [ex| λs. λz. s z |]) one
-    print $ assert (three == full_beta [ex| $e:plus $e:one $e:two|]) three    
+    print $ assert (three == full_beta [ex| $e:plus $e:one $e:two|]) three
+
+    let i = 100
+    print $ assert (Just i == (fromChurch .toChurch) i) (Just i)
